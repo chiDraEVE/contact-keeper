f74b2c89f8a44908a70a75deb64e9535f718c2ec
chidraeve <lukas.mainka@clubitsolutions.de>
2021-01-01 00:22:24 +0100

    40. Backend Routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// @route   POST    api/users
+// @desc    Register a user
+// @access  Public
+router.post('/', (req,res) => {
+  res.send("Register a user")
+})
 
-module.exports = router;
+module.exports = router
