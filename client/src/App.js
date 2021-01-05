9f89ae91c17b070e3ab380577b224ccf0f36de1e
chidraeve <lukas.mainka@clubitsolutions.de>
2021-01-05 11:36:02 +0100

    60. Auth Context & Initial State

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,39 @@
-import "./App.css";
-import { library } from "@fortawesome/fontawesome-svg-core";
+import "./App.css"
+import { library } from "@fortawesome/fontawesome-svg-core"
 import {
-  faEnvelopeOpen,
-  faIdCardAlt,
-  faPhone,
-  fas,
-} from "@fortawesome/free-solid-svg-icons";
-import Navbar from "./components/layout/Navbar";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Fragment } from "react";
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import ContactState from "./context/contact/ContactState";
+	faEnvelopeOpen,
+	faIdCardAlt,
+	faPhone,
+	fas
+} from "@fortawesome/free-solid-svg-icons"
+import Navbar from "./components/layout/Navbar"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { Fragment } from "react"
+import Home from "./components/pages/Home"
+import About from "./components/pages/About"
+import ContactState from "./context/contact/ContactState"
+import AuthState from "./context/auth/AuthState"
 
-library.add(fas, faIdCardAlt, faEnvelopeOpen, faPhone);
+library.add(fas, faIdCardAlt, faEnvelopeOpen, faPhone)
 
 const App = () => {
-  return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
-  );
-};
+	return (
+		<AuthState>
+			<ContactState>
+				<Router>
+					<Fragment>
+						<Navbar />
+						<div className="container">
+							<Switch>
+								<Route exact path="/" component={Home} />
+								<Route exact path="/about" component={About} />
+							</Switch>
+						</div>
+					</Fragment>
+				</Router>
+			</ContactState>
+		</AuthState>
+	)
+}
 
-export default App;
+export default App
