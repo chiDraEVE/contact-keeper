2b6115a46a8edc2fbc4d23ac68ba5b9ebbb76c56
chidraeve <lukas.mainka@clubitsolutions.de>
2021-01-05 19:14:17 +0100

    65. User Login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,24 @@
-import React, { useState } from "react"
+import React, { useState, useEffect, useContext } from "react"
+import AuthContext from "../../context/auth/authContext"
+import AlertContext from "../../context/alert/alertContext"
 
-const Login = () => {
+const Login = props => {
+	const alertContext = useContext(AlertContext)
+	const authContext = useContext(AuthContext)
+
+	const { setAlert } = alertContext
+	const { login, error, clearErrors, isAuthenticated } = authContext
+
+	useEffect(() => {
+		if (isAuthenticated) props.history.push("/")
+
+		if (error === "Invalid Credentials") {
+			setAlert(error, "danger")
+			clearErrors()
+		}
+		// eslint-disable-next-line
+	}, [error, isAuthenticated, props.history])
+
 	const [user, setUser] = useState({
 		email: "",
 		password: ""
@@ -12,7 +30,14 @@ const Login = () => {
 
 	const onSubmit = e => {
 		e.preventDefault()
-		console.log("Login Submit")
+		if (email === "" || password === "")
+			setAlert("Please fill in all fields", "danger")
+		else {
+			login({
+				email,
+				password
+			})
+		}
 	}
 
 	return (
@@ -20,10 +45,16 @@ const Login = () => {
 			<h1>
 				Account <span className="text-primary">Login</span>
 			</h1>
-			<form onSubmit={onsubmit}>
+			<form onSubmit={onSubmit}>
 				<div className="form-group">
 					<label htmlFor="email">Email Address</label>
-					<input type="email" name="email" value={email} onChange={onchange} />
+					<input
+						type="email"
+						name="email"
+						value={email}
+						onChange={onChange}
+						required
+					/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="password">Password</label>
@@ -31,7 +62,8 @@ const Login = () => {
 						type="password"
 						name="password"
 						value={password}
-						onChange={onchange}
+						onChange={onChange}
+						required
 					/>
 				</div>
 				<input
